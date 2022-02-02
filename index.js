8bd41866dc1bca0638cad5ed889e1033746a46b2
Betelihem <Betelihem>
2022-02-01 19:49:21 -0800

    commit tree b02ccc21494b768c7103c054ec134efded20c86a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0], drivers[1]];
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return [drivers[2], drivers[3]];
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(ride) {
+    return function(fare) {
+        return ride * fare;
+    }
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, other) {
+    if (other === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    } else if (other === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers);
+    }
+}
